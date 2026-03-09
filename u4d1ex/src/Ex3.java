c018fff7609a2cb03b0ec0cfd16915e25a036ef7
Samuel-Valentini <samuelvitalia@gmail.com>
2026-03-09 16:16:06 +0100

    done

--- u4d1ex/src/Ex3.java
+++ u4d1ex/src/Ex3.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class Ex3 {
     static void main() {
         rectanglePerimeter();
+
+        Scanner watcher = new Scanner(System.in);
+        System.out.println("Inserisci un numero, riceverai 0 se pari o 1 se dispari");
+        int num = Integer.parseInt(watcher.nextLine());
+        System.out.println(evenOdd(num));
+        System.out.println("Inserisci 3 lati di un triangolo e verrà calcolata l'area, un lato per riga, anche dotato di decimali");
+        double s1 = Double.parseDouble(watcher.nextLine());
+        double s2 = Double.parseDouble(watcher.nextLine());
+        double s3 = Double.parseDouble(watcher.nextLine());
+        System.out.println("Ora inserisci l'unità di misura");
+        String unitOfMeasurement = " " + watcher.nextLine();
+        System.out.println("L'area è: " + triangleArea(s1, s2, s3) + unitOfMeasurement);
+
+
     }
 
     static void rectanglePerimeter() {
@@ -15,5 +29,16 @@ public class Ex3 {
         System.out.println("Il perimetro è " + (side1 * 2 + side2 * 2) + " " + unitOfMeasurement);
 
 
+    }
+
+    static int evenOdd(int num) {
+        return num % 2;
+    }
+
+    static double triangleArea(double s1, double s2, double s3) {
+
+        double semiperimeter = (s1 + s2 + s3) / 2;
+
+        return Math.sqrt(semiperimeter * (semiperimeter - s1) * (semiperimeter - s2) * (semiperimeter - s3));
     }
 }
